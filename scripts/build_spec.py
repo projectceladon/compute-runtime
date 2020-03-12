18db07620b6b02b7a539530ecdae6fb5e5b1c416
Wan Shuang <shuang.wan@intel.com>
2020-03-12 13:51:40 +0800

    Upgraded Python2 scripts
    Upgraded Python2 scripts to Python 3 using 2to3 tool.
    This is to mitigrate security risks of Python2 deprecation.
    
    Signed-off-by: Wan Shuang <shuang.wan@intel.com>
    Tracked-On: OAM-90214

--- scripts/build_spec.py
+++ scripts/build_spec.py
@@ -16,11 +16,11 @@ import sys
 import yaml
 
 if len(sys.argv) < 4:
-    print "ERROR! invalid number of parameters"
-    print
-    print "Usage:"
-    print "  ./scripts/build_spec.py <spec.in> <version> <revision>"
-    print
+    print("ERROR! invalid number of parameters")
+    print()
+    print("Usage:")
+    print("  ./scripts/build_spec.py <spec.in> <version> <revision>")
+    print()
     sys.exit(1)
 
 repo = git.Repo(".")
@@ -34,16 +34,16 @@ pkg_version = "%s.%02d.%s" %(str(cd.isocalendar()[0])[-2:], cd.isocalendar()[1],
 with open(sys.argv[1], 'r') as f:
     for line in f.readlines():
         if not re.match(".*__NEO_COMMIT_ID__$", line.strip()) is None:
-            print "%s" % (line.rstrip().replace("__NEO_COMMIT_ID__", "%s" % neo_revision))
+            print("%s" % (line.rstrip().replace("__NEO_COMMIT_ID__", "%s" % neo_revision)))
             continue
 
         if not re.match(".*__NEO_PACKAGE_VERSION__$", line.strip()) is None:
-            print "%s" % (line.rstrip().replace("__NEO_PACKAGE_VERSION__", "%s" % pkg_version))
+            print("%s" % (line.rstrip().replace("__NEO_PACKAGE_VERSION__", "%s" % pkg_version)))
             continue
 
         if not re.match(".*__NEO_PACKAGE_RELEASE__.*", line.strip()) is None:
-            print "%s" % (line.rstrip().replace("__NEO_PACKAGE_RELEASE__", "%s" % sys.argv[3]))
+            print("%s" % (line.rstrip().replace("__NEO_PACKAGE_RELEASE__", "%s" % sys.argv[3])))
             continue
 
-        print line.rstrip()
+        print(line.rstrip())
 
