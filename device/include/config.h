2fbe0ccdda89ef4d577d98bb80cddb9c0003f67a
Mi, Yanfeng <yanfeng.mi@intel.com>
2024-04-22 14:35:01 +0800

    Add built-in binaries for android build
    Tracked-On: OAM-117845
    
    Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>

---
+++ device/include/config.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018-2023 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define CL_CACHE_LOCATION "cl_cache"
+#define NEO_ARCH "x64"
+
+#endif /* CONFIG_H */
---
+++ device/include/driver_version.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018-2021 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef DRIVER_VERSION_H
+#define DRIVER_VERSION_H
+
+#define NEO_OCL_DRIVER_VERSION 24.09.028717
+#define NEO_REVISION "028717"
+
+#endif /* DRIVER_VERSION_H */
