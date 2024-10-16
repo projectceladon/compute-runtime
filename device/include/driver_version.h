953d4a7170f3ebb71b9153acfb9af8556e0e2cba
JeevakaPrabu <jeevaka.badrappan@intel.com>
2024-10-16 15:59:36 +0000

    Add built-in binaries for android build
    Tracked-On: OAM-117845
    Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>
    Signed-off-by: JeevakaPrabu <jeevaka.badrappan@intel.com>

---
+++ device/include/driver_version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef DRIVER_VERSION_H
+#define DRIVER_VERSION_H
+
+#define NEO_OCL_DRIVER_VERSION 24.31.030508
+#define NEO_REVISION "030508"
+
+#define NEO_VERSION_BUILD 30508
+#define NEO_VERSION_HOTFIX 0
+
+#endif /* DRIVER_VERSION_H */
---
+++ device/include/test_files_setup.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2022 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef TEST_FILES_SETUP_H
+#define TEST_FILES_SETUP_H
+
+#define NEO_SHARED_BUILTINS_DIR "/home/celadon/workspace/github_clone/ws-opencl-runtime/compute-runtime/shared/source/built_ins/kernels"
+#define NEO_SHARED_TEST_FILES_DIR "/home/celadon/workspace/github_clone/ws-opencl-runtime/compute-runtime/shared/test/common/test_files"
+#define NEO_OPENCL_TEST_FILES_DIR "/home/celadon/workspace/github_clone/ws-opencl-runtime/compute-runtime/opencl/test/unit_test/test_files"
+
+#endif /* TEST_FILES_SETUP_H */
