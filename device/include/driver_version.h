d8097b57ede523dd2cffe6d2dca63ef8eab6ba20
JeevakaPrabu <jeevaka.badrappan@intel.com>
2024-10-17 15:42:18 +0000

    Add built-in binaries for tag 24.35.30872.22
    Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>
    Signed-off-by: JeevakaPrabu <jeevaka.badrappan@intel.com>

---
+++ device/include/driver_version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef DRIVER_VERSION_H
+#define DRIVER_VERSION_H
+
+#define NEO_OCL_DRIVER_VERSION 24.35.030872
+#define NEO_REVISION "030872"
+
+#define NEO_VERSION_BUILD 30872
+#define NEO_VERSION_HOTFIX 0
+
+#endif /* DRIVER_VERSION_H */
