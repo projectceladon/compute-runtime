964508cf9bd36a0ec3e955bb6788f30e89c35fe7
Mi, Yanfeng <yanfeng.mi@intel.com>
2025-05-14 19:49:24 +0800

    Add prebuilt kernel binary for android build
    gen12+ kernels
    
    Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>

---
+++ device/include/config.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2018-2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define NEO_ARCH "x64"
+
+#endif /* CONFIG_H */
---
+++ device/include/driver_version.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2018-2024 Intel Corporation
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ */
+
+#ifndef DRIVER_VERSION_H
+#define DRIVER_VERSION_H
+
+#define NEO_OCL_DRIVER_VERSION 25.13.033276
+#define NEO_REVISION "033276"
+
+#define NEO_VERSION_BUILD 33276
+#define NEO_VERSION_HOTFIX 0
+
+#endif /* DRIVER_VERSION_H */
