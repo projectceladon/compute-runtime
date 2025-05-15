f993cb87fdf1a6c4f879689c4d82b86858fc7370
Mi, Yanfeng <yanfeng.mi@intel.com>
2025-05-15 11:47:37 +0800

    Refactor opencl compiler cache dir logic based on Android
    Signed-off-by: Mi, Yanfeng <yanfeng.mi@intel.com>

--- shared/source/compiler_interface/linux/os_compiler_cache_helper.cpp
+++ shared/source/compiler_interface/linux/os_compiler_cache_helper.cpp
@@ -17,6 +17,13 @@
 #include <dlfcn.h>
 #include <link.h>
 
+
+#include <binder/IPCThreadState.h>
+#include <cutils/multiuser.h>
+#include <string>
+
+using ::android::IPCThreadState;
+
 namespace NEO {
 bool createCompilerCachePath(std::string &cacheDir) {
     if (NEO::pathExists(cacheDir)) {
@@ -45,6 +52,63 @@ bool checkDefaultCacheDirSettings(std::string &cacheDir, NEO::EnvironmentVariabl
     cacheDir = reader.getSetting("XDG_CACHE_HOME", emptyString);
 
     if (cacheDir.empty()) {
+#ifdef ANDROID
+        cacheDir = "/data/data";
+        cacheDir = joinPath(cacheDir, getprogname());
+        if (!NEO::pathExists(cacheDir)) {
+                NEO::SysCalls::mkdir(cacheDir);
+        }
+
+        cacheDir = joinPath(cacheDir, ".cache/");
+        if (!NEO::pathExists(cacheDir)) {
+            NEO::SysCalls::mkdir(cacheDir);
+        }
+
+         // In case /data/data/progname/.cache creation fails, which can happen in case of multiuser or due to service
+        //  running in shell due to permission issue, try multiuser path /data/user/userid/progname/.cache
+        if (!NEO::pathExists(cacheDir)) {
+            PRINT_DEBUG_STRING(NEO::debugManager.flags.PrintDebugMessages.get(), stdout, "unable to create cache in:  %s\n\n",
+                           cacheDir.c_str());
+            cacheDir = "/data/user";
+            //get current userid
+            IPCThreadState* ipc = IPCThreadState::self();
+            const int32_t uid = ipc->getCallingUid();
+            userid_t user = multiuser_get_user_id(uid);
+
+            cacheDir = joinPath(cacheDir, (std::to_string(user)).c_str());
+            cacheDir = joinPath(cacheDir, getprogname());
+
+            cacheDir = joinPath(cacheDir, ".cache/");
+            if (!NEO::pathExists(cacheDir)) {
+                NEO::SysCalls::mkdir(cacheDir);
+            }
+            PRINT_DEBUG_STRING(NEO::debugManager.flags.PrintDebugMessages.get(), stdout, "Trying to create cache in: %s\n\n",
+                                cacheDir.c_str());
+        }
+
+        //now if cache dir is still not created use /data/local/tmp/cache/ as fallback cache dir
+        if (!NEO::pathExists(cacheDir)) {
+            PRINT_DEBUG_STRING(NEO::debugManager.flags.PrintDebugMessages.get(), stdout, "unable to create cache in: %s\n\n",
+                                cacheDir.c_str());
+            cacheDir = "/data/local/tmp/cache";
+            if (!NEO::pathExists(cacheDir)) {
+                NEO::SysCalls::mkdir(cacheDir);
+            }
+
+            cacheDir = joinPath(cacheDir, getprogname());
+            if (!NEO::pathExists(cacheDir)) {
+                NEO::SysCalls::mkdir(cacheDir);
+            }
+
+            cacheDir = joinPath(cacheDir, ".cache/");
+            if (!NEO::pathExists(cacheDir)) {
+                NEO::SysCalls::mkdir(cacheDir);
+            }
+            PRINT_DEBUG_STRING(NEO::debugManager.flags.PrintDebugMessages.get(), stdout, "creating temporay cache in:  %s\n\n",
+                                cacheDir.c_str());
+        }
+
+#else
         cacheDir = reader.getSetting("HOME", emptyString);
         if (cacheDir.empty()) {
             return false;
@@ -55,7 +119,7 @@ bool checkDefaultCacheDirSettings(std::string &cacheDir, NEO::EnvironmentVariabl
         if (!NEO::pathExists(cacheDir)) {
             NEO::SysCalls::mkdir(cacheDir);
         }
-
+#endif
         return createCompilerCachePath(cacheDir);
     }
 
